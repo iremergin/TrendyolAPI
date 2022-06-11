23f0c130d4de578e6dde0a3ec09a51e5dad4f048
iremergin <iremergin58@gmail.com>
2022-06-11 20:03:25 +0300

    first commit

---
+++ API/API/Controllers/KategoriController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KategoriController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> kategoriListeleme(string kategori)
+        {
+            WebRequest requests = HttpWebRequest.Create("https://api.trendyol.com/sapigw/brands/by-name?name=" + kategori);
+            WebResponse responses = requests.GetResponse();
+            StreamReader readers = new StreamReader(responses.GetResponseStream());
+            string Kategori_JSOns = readers.ReadToEnd();
+
+
+
+            return Ok(Kategori_JSOns);
+        }
+    }
+}
---
+++ API/API/Controllers/MarkaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarkaController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> markaListeleme(string marka)
+        {
+            WebRequest request = HttpWebRequest.Create("https://api.trendyol.com/sapigw/brands/by-name?name=" + marka);
+            WebResponse response = request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string Marka_JSOn = reader.ReadToEnd();
+
+
+
+            return Ok(Marka_JSOn);
+        }
+
+    }
+}
